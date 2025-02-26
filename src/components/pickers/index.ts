db1bbbeecc27c727b33a2dabbe5168e32f0ce42e
Henri Fardeau <fardeauhenri@gmail.com>
2025-02-26 09:17:25 +0100

    fix: export name

--- src/components/pickers/index.ts
+++ src/components/pickers/index.ts
@@ -2,5 +2,5 @@ export * from './color-select-picker';
 export * from './color-swatch-picker';
 export * from './image-picker';
 export * from './select-picker';
-export * from './size-picker';
+export * from './list-picker';
 export * from './switch-pickers';
